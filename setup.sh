78c1c6899565752fafacf09b43fc7bad39c8ffa0
Hoo-dkwozD <hoonderrick@gmail.com>
2024-04-02 17:54:33 +0800

    Update of setup shell file;
    Signed-off-by: Hoo-dkwozD <hoonderrick@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,13 +1,11 @@
 #!/bin/bash -xeu
 
-apt update
 apt install systemctl wget curl -y
 apt install apache2 -y
 apt install php php-mysqli -y
 apt install php-curl php-mbstring -y
 wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
 mv composer.phar /usr/local/bin/composer
-cd Boomerang-EC2
 mv * /var/www/html/
 cd /var/www/html/
 rm -rf ~/Boomerang-EC2
