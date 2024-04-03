b68d1e79f5b6468899c99f36dc41190141230d6d
Hoo-dkwozD <hoonderrick@gmail.com>
2024-04-03 16:48:22 +0800

    Debug for NoSQL 4;
    Signed-off-by: Hoo-dkwozD <hoonderrick@gmail.com>

--- loginLogic.php
+++ loginLogic.php
@@ -29,6 +29,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             ]
         ]);
         $user = $result['Items'][0] ? count($result['Items']) > 0 : NULL;
+        echo print_r($user);
     } catch (AwsException $e) {
         $user = NULL;
         echo $e->getAwsErrorMessage();
