73745e5625f8cc043cb4d0034fa1e5ade8ef9862
Hoo-dkwozD <hoonderrick@gmail.com>
2024-04-03 16:01:38 +0800

    Update to account for NoSQL 2;
    Signed-off-by: Hoo-dkwozD <hoonderrick@gmail.com>

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 apt install systemctl wget curl -y
 apt install apache2 -y
 apt install php php-mysqli -y
-apt install php-curl php-mbstring -y
+apt install php-curl php-mbstring php-xml -y
 wget https://raw.githubusercontent.com/composer/getcomposer.org/76a7060ccb93902cd7576b67264ad91c8a2700e2/web/installer -O - -q | php -- --quiet
 mv composer.phar /usr/local/bin/composer
 mv * /var/www/html/
